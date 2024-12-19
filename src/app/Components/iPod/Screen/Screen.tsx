e6fbc889d42c890577bdce30219fb5823b6479ff
Vincent <Vincent.ahlin99@gmail.com>
2024-12-19 12:52:17 +0100

    Styling and preparation to build out a structured screen component to handle dynamic content

--- src/app/Components/iPod/Screen/Screen.tsx
+++ src/app/Components/iPod/Screen/Screen.tsx
@@ -1,28 +1,45 @@
 import { MenuItem } from "@/types/iPod/Screen";
+import "./Screen.css";
 
 interface ScreenProps {
   menuItems: MenuItem[];
   selectedIndex: number;
+  hoveredIndex?: number | null;
   onMenuSelect: (item: MenuItem) => void;
+  onMenuItemHover?: (index: number) => void;
 }
-
-
 
 export const Screen: React.FC<ScreenProps> = ({
   menuItems,
   selectedIndex,
+  hoveredIndex,
   onMenuSelect,
+  onMenuItemHover,
 }) => {
+  const handleItemClick = (item: MenuItem) => {
+    onMenuSelect(item);
+  };
+
+  const handleItemHover = (index: number) => {
+    if (onMenuItemHover) {
+      onMenuItemHover(index);
+    }
+  };
+
   return (
     <div className="screen">
       <div className="menu-list">
         {menuItems.map((item, index) => (
           <div
-            key={item.label}
+            key={`${item.label}-${index}`}
             className={`menu-item ${
               index === selectedIndex ? "menu-item--selected" : ""
-            }`}
-            onClick={() => onMenuSelect(item)}
+            } ${hoveredIndex === index ? "menu-item--hovered" : ""}`}
+            onClick={() => handleItemClick(item)}
+            onMouseEnter={() => handleItemHover(index)}
+            onMouseLeave={() => handleItemHover(selectedIndex)}
+            role="button"
+            tabIndex={0}
           >
             {item.label}
           </div>
